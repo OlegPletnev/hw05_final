ab33245385821da0452a80bd24db490dc4d1e8b5
OlegPletnev <oleg-pletnev@yandex.ru>
2021-11-21 14:55:50 +0300

    Перед третьим ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -81,5 +81,7 @@ class Follow(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'author'], name='unique_follow')
+            models.UniqueConstraint(
+                fields=['user', 'author'], name='unique_follow'
+            )
         ]
